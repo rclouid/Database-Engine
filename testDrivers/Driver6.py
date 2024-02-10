7e1b4a16eb2f84a5b4e7739a78c5edffa39ad956
rclouid <145714489+rclouid@users.noreply.github.com>
2024-02-10 02:16:07 -0500

    Update Driver6.py

--- testDrivers/Driver6.py
+++ testDrivers/Driver6.py
@@ -1,3 +1,33 @@
+"""
+STUDENT(SID:INTEGER,SNAME:VARCHAR,PHONE:INTEGER,MAJOR:VARCHAR,GPA:DECIMAL)
+Number of tuples:4
+
+1111:Robert Adams:1234:Computer Science:4.0:
+1112:Charles Bailey:5656:Computer Science:3.5:
+1113:David Beatle:1212:Mathematics:3.5:
+1114:Graham Gooch:5678:Computer Science:3.5:
+
+PROJECT_SID_GPA_STUDENT(SID:INTEGER,GPA:DECIMAL)
+Number of tuples:4
+
+1111:4.0:
+1112:3.5:
+1113:3.5:
+1114:3.5:
+
+PROJECT_MAJOR_GPA_STUDENT(MAJOR:VARCHAR,GPA:DECIMAL)
+Number of tuples:3
+
+Computer Science:4.0:
+Computer Science:3.5:
+Mathematics:3.5:
+
+PROJECT_MAJOR_STUDENT(MAJOR:VARCHAR)
+Number of tuples:2
+
+Computer Science:
+Mathematics:
+"""
 from Database import *
 from Relation import *
 from Tuple import *
