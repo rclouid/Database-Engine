d0d0500956376b1e39ced3040c3a03641a748230
rclouid <145714489+rclouid@users.noreply.github.com>
2024-02-10 02:14:28 -0500

    Update Driver2.py

--- testDrivers/Driver2.py
+++ testDrivers/Driver2.py
@@ -1,3 +1,34 @@
+"""
+BAR(BNAME:VARCHAR)
+Number of tuples:4
+
+Jillians:
+Dugans:
+ESPN Zone:
+Charlies:
+
+DRINKER(DNAME:VARCHAR)
+Number of tuples:5
+
+John:
+Peter:
+Donald:
+Jeremy:
+Clark:
+
+SELLS(BAR:VARCHAR,BEER:VARCHAR,PRICE:INTEGER)
+Number of tuples:9
+
+Jillians:Bud:6:
+Jillians:Michelob:6:
+Jillians:Heineken:8:
+Dugans:Bud:9:
+Dugans:Michelob:10:
+Dugans:Fosters:12:
+ESPN Zone:Fosters:9:
+Charlies:Heineken:10:
+Charlies:Foster:10:
+"""
 from Database import *
 from Relation import *
 import sys
