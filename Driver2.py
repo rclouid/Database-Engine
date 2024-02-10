93f9d57dfd810d7e2a52d01e67e7492dd91c4761
rclouid <145714489+rclouid@users.noreply.github.com>
2024-02-10 01:47:52 -0500

    Update Driver2.py

--- Driver2.py
+++ Driver2.py
@@ -1,7 +1,37 @@
 from Database import *
 from Relation import *
 import sys
+"""
+BAR(BNAME:VARCHAR)
+Number of tuples:4
 
+Jillians:
+Dugans:
+ESPN Zone:
+Charlies:
+
+DRINKER(DNAME:VARCHAR)
+Number of tuples:5
+
+John:
+Peter:
+Donald:
+Jeremy:
+Clark:
+
+SELLS(BAR:VARCHAR,BEER:VARCHAR,PRICE:INTEGER)
+Number of tuples:9
+
+Jillians:Bud:6:
+Jillians:Michelob:6:
+Jillians:Heineken:8:
+Dugans:Bud:9:
+Dugans:Michelob:10:
+Dugans:Fosters:12:
+ESPN Zone:Fosters:9:
+Charlies:Heineken:10:
+Charlies:Foster:10:
+"""
 def main():
     db = Database()
     db.initializeDatabase(sys.argv[1])
